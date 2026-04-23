0b98fec72b670049d6ad0ba10486c549e485a147
akshadasanap7 <akshadasanap297@gmail.com>
2026-04-23 18:55:37 +0530

    added the

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 p-4 flex justify-between text-white">
-      <h1 className="font-bold text-xl">PlaceMentor AI</h1>
-      <div className="space-x-4">
+    <nav className="navbar">
+      <h2 className="logo">PlaceMentor AI</h2>
+
+      <div className="nav-links">
         <Link to="/">Dashboard</Link>
         <Link to="/upload">Resume Upload</Link>
       </div>
